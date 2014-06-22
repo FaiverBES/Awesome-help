b8d33cf2fae2057d49ae5277dc0a0bf4a3e59945
FaiverBES <evg.bichan@yandex.ru>
2014-06-23 02:32:54 +0700

    some modified and add icon

--- help.lua
+++ help.lua
@@ -8,13 +8,17 @@ local capi = {
 	mouse = mouse,
 	screen = screen
 }
+
+local maxLength = 0      --variable for max length to '-'
+local nf=nil             -- index for notify
+
 --Here you might add your terminal (class, not a name)
 local termClass = { "UXTerm", "Xterm", "XTerm","Konslole", "URxvt", "Rxvt" }
 
 module("help")
 
-local nf=nil
 -- get client name by c.class, or c.pid
+-- call from clientkey section, accept value is active client
 function getClientName(c)
 	local cname=nil
 	if (isTerminal(c.class)) then
@@ -24,20 +28,25 @@ function getClientName(c)
 	else
 		cname = tostring(c.class)
 	end
+	--nf=displayHelp(cname)
+	return displayHelp(cname)
+end
+
+--this function call from helpwwidget in rc.lua
+function displayHelp(cname)
 	local fname = awful.util.getdir("config") .. "/help/data/" .. cname     --here you must change path, if not working
 	if awful.util.file_readable(fname) then
 		local myData = readFile(fname)
-		local myData = markupData (myData) 
+		myData = splitStr (myData)
+		myData = markupData(myData)
 		--nf = naughty.notify ({title = '<span weight="bold" color="#00FF00">' .. "Подсказка для:      " .. cname .. '</span>', text = myData, timeout=60,screen=capi.mouse.screen})
 		nf = naughty.notify ({title = '<span weight="bold" color="#00FF00">' .. "Help for:      " .. cname .. '</span>', text = myData, timeout=60,screen=capi.mouse.screen})
 	else
 		--nf = naughty.notify ({title = '<span weight="bold" color="#FF0000">' .. "Файл с подсказкой для: " .. cname.. " не найден" .. '</span>' ,screen=capi.mouse.screen})
 		nf = naughty.notify ({title = '<span weight="bold" color="#FF0000">' .. "Can't find help file for: " .. cname .. '</span>' ,screen=capi.mouse.screen})
 	end
-	
 	return nf
-end 
-
+end
 --function verify, terminal or not
 function isTerminal(clientClass)
 	for _,v in pairs (termClass) do
@@ -56,49 +65,63 @@ function readFile(file)
 	return myStr
 end
 
---this function markup text
-function markupData (str)
-	local result,tmp = "",""
-	local markData= {}
-	local strLength = string.len(str)
-	local curPos, curLast, maxLength = 0,0,0
-	--for more convenient processing, I separated the text for the lines ( \n)
-	while (curPos<strLength) 
-		do
-			curPos,_ = string.find (str,"\n",curPos)
-			markData[#markData+1] = string.sub (str, curLast+1, curPos)
-			tmp = markData[#markData]
-			local posTir,_ = string.find (tmp, "-")
-			if posTir then
-				if (posTir>maxLength) then
-					maxLength =posTir 
-				end
-			end
-			curLast = curPos
-			if curPos then
-				curPos = curPos + 2
-			end
+--separate text by end of line
+function splitStr(str)
+  local result = {}  
+  local lastPos = 1
+  local s, e, cap = string.find(str,"(.-)"..'\n', 1)
+  while s do
+    if s ~= 1 or cap ~= "" then
+		result[#result+1] = cap
+		alignmentPos(cap)
+    end
+    lastPos = e+1
+    s, e, cap = string.find(str,"(.-)"..'\n', lastPos)
+  end
+  if lastPos <= #str then
+    cap = string.sub(str,lastPos)
+	result[#result+1] = cap
+	alignmentPos(cap)
+  end
+  return result
+end
+
+--this function calculate max position of '-', for alignment text
+function alignmentPos(str)
+	local posTir,_ = string.find (str, "-")
+	if posTir then
+		if (posTir>maxLength) then
+			maxLength =posTir
+		end
 	end
-	local posSubKey= nil
-	for k,v in  pairs (markData) do
-		posSubKey,_ = string.find (v, "-")
-		if posSubKey then
+end
+
+--this function markup text, and align text by '-'
+function markupData (str)
+	local result = ""
+	local posCentre= nil
+	for k,v in  pairs (str) do
+		posCentre,_ = string.find (v, "-")
+		if posCentre then
 			v = string.gsub (v, "-", "</span>-",1)
-			if (posSubKey < maxLength) then
-				local increm = maxLength - posSubKey
-				for  i=1,increm do  --align by center
+			if (posCentre < maxLength) then
+				local addSpace = maxLength - posCentre
+				for  i=1,addSpace  do  --align by center
 					v = " " .. v
 				end
 			end
 			v =  '<span font="DejaVu Sans Mono 10" weight="bold" color="#FF4500">' .. v 
+			v = v .. '\n'
 		end
-		if  string.find (v, "=") then
+		if  string.find (v, "==") then
 			v = string.gsub (v, "==", '<span font="DejaVu Sans Mono 10" weight="bold" color="#00FFFF">', 1)
 			v = string.gsub (v, "==", '</span>',1)
+			v = v .. '\n'
 		end   
         if string.find (v, "\'") then
 			v = string.gsub (v, "\'\'", '<span font="DejaVu Sans Mono 10" weight="bold" color="#FFFF00">', 1)
 			v = string.gsub (v, "\'\'", '</span>',1)
+			v = v .. '\n'
 		end   
 			result = result .. v
 	end
